1774be3351f3e929fe64d3358b777dfc96c455bd
Dayanand Saraswati <dsaraswati@atlassian.com>
2020-07-08 21:23:25 +0530

    Startup project ready

---
+++ src/main/java/com/jsnovice/samplemvn/SampleMvnApplication.java
@@ -0,0 +1,13 @@
+package com.jsnovice.samplemvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsnovice/samplemvn/SampleMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsnovice.samplemvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
